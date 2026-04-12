8e7d5e59667661951d2123978e2eecf39379dd65
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-04-12 19:27:32 +0100

    simple test of the arena

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <print>
+//--------------------
+#include "types.h"
+#include "arena.h"
+
+size_t to_mb(size_t val)
+{
+    return val / 1024 / 1024;
+}
 
 int main()
 {
-    std::print("Hello World\n");
+    const i32 MB = 1024 * 1024 * 3; 
+    my::arena arena(MB);
+    
+    std::print("capacity: {0} MB\n offset: {1}\n", to_mb(arena.capacity()), arena.used());
+    i32* x = arena.allocate<i32>(1);
+
+    std::print("Value: {0}\n", *x);
+    std::print("capacity: {0} MB\n offset: {1}\n", to_mb(arena.capacity()), arena.used());
+
+    std::print("RESET\n");
+
+    arena.reset();
+
+    std::print("capacity: {0} MB\n offset: {1}\n", to_mb(arena.capacity()), arena.used());
     return 0;
-}
+};
