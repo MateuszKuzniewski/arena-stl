562b6bca025f941a283d27677d110934f2676c39
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-04-17 16:28:46 +0100

    added tests of the vector implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,75 @@
 #include <print>
 #include <cstring>
+#include <vector>
 //--------------------
 #include "types.h"
 #include "arena.h"
 #include "string.h"
+#include "vector.h"
+
+struct vector_data
+{
+    i32 x;
+    i32 y;
+    i32 z;
+};
 
 size_t to_mb(const size_t val)
 {
     return val / 1024 / 1024;
 }
 
-
-int main()
+int main() 
 {
     const i32 MB = 1024 * 1024 * 3; 
     my::arena arena(MB);
 
+    //////////////// STRING //////////////////////////
+    std::print("\nSTRING\n");
     my::string s(arena, "Hello World");
     const char* cstr = s.c_str();
     size_t size = s.size();
     char c = s[1];
+    std::print("word: {0} | size: {1} | 2nd char: {2}\n", cstr, size, c);
+ 
 
-    std::print("{0} | size: {1} | 2nd character: {2}\n", cstr, size, c);
-    std::print("arena size: {0}\n", arena.used());
+    //////////////// VECTOR //////////////////////////
+    std::print("\nVECTOR - primitive type\n");
+    my::vector<int> v(arena, 5);
+    for (int i = 0; i <= 4; i++)
+    {
+        v.push_back(i);
+    }
     
+    for(auto i : v)
+    {
+        std::print("item: {0}\n", i);
+    }
+
+
+    std::print("\nVECTOR - non-trivial type\n");
+    my::vector<vector_data> vd(arena, 2);
+    vector_data vds
+    {
+        .x = 1,
+        .y = 2,
+        .z = 3
+    };
+
+    vd.push_back(vds);
+    vd.emplace_back(5,6,7);
+
+    for(auto item : vd)
+    {
+        std::print("x: {0}, y: {1}, z: {2}\n", item.x, item.y, item.z);
+    }
+
+
+    std::print("arena size: {0}\n", arena.used());
+
     arena.reset();
 
     std::print("arena size: {0}\n", arena.used());
 
-    // std::vector<int> a;
-    // std::print("capacity: {0} MB\n offset: {1}\n", to_mb(arena.capacity()), arena.used());
-    // i32* x = arena.allocate<i32>(1);
-    //
-    // std::print("Value: {0}\n", *x);
-    // std::print("capacity: {0} MB\n offset: {1}\n", to_mb(arena.capacity()), arena.used());
-    //
-    // std::print("RESET\n");
-    //
-    // arena.reset();
-    //
-    // std::print("capacity: {0} MB\n offset: {1}\n", to_mb(arena.capacity()), arena.used());
-    //
     return 0;
 };
