1ad74c69eee46e617be510ac9c17ac4a62ab9f28
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-04-20 13:19:00 +0100

    added list test

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "arena.h"
 #include "string.h"
 #include "array.h"
+#include "list.h"
 
 struct array_data
 {
@@ -12,6 +13,7 @@ struct array_data
     i32 y;
     i32 z;
 };
+
 
 size_t to_mb(const size_t val)
 {
@@ -23,6 +25,33 @@ int main()
     const i32 MB = 1024 * 1024 * 3; 
     my::arena arena(MB);
 
+    //////////////// LIST //////////////////////////
+    std::print("\nLIST\n");
+    my::list<int> list(arena);
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_front(5);
+    list.push_front(6);
+    list.push_front(7);
+
+    std::print("list size: {0}\n", list.size());
+
+    for (auto& item : list)
+    {
+        std::print("item: {0}\n", item);
+    }
+
+    my::list<array_data> secondList(arena);
+
+    array_data ad {1, 2 ,3};
+    secondList.push_back(ad);
+
+    for (auto& item : secondList)
+    {
+        std::print("x: {0}, y: {1}, z: {2}\n", item.x, item.y, item.z);
+    }
     //////////////// STRING //////////////////////////
     std::print("\nSTRING\n");
     my::string s(arena, "Hello World");
@@ -30,6 +59,7 @@ int main()
     auto sub = s.substr(3, 4);
     std::print("word: {0} | size: {1} | 2nd char: {2}\n", s.c_str(), s.size(), s[1]);
     std::print("{0}\n", sub->c_str());
+
 
     //////////////// ARRAY //////////////////////////
     std::print("\nARRAY- primitive type\n");
@@ -58,7 +88,7 @@ int main()
     vd.push_back(vds);
     vd.emplace_back(5,6,7);
 
-    for (auto item : vd)
+    for (const auto& item : vd)
     {
         std::print("x: {0}, y: {1}, z: {2}\n", item.x, item.y, item.z);
     }
